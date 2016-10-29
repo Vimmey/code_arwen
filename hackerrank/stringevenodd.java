b31b3e893d2822fb57dcbb678cc3c16231ed2415
Vimmey <vmy.chopra@gmail.com>
2016-10-29 10:08:19 +0530

    Hackerank Day 6

--- hackerrank/stringevenodd.java
+++ hackerrank/stringevenodd.java
@@ -1,20 +1,25 @@
 import java.io.*;
+import java.util.*;
+
 public class stringevenodd {
-       public static void main(String[] args) throws IOException {
+
+    public static void main(String[] args) throws IOException {
            BufferedReader br = new BufferedReader( new InputStreamReader(System.in));
-           String s = br.readLine();
-           String even = "";
-           String odd = "";
-
-            for (int i =0; i<s.length(); i++) {
-           		if (i %2 == 0) {
-           			even = even + s.charAt(i);
-           		} else {
-           			odd = odd + s.charAt(i);
-           		}
-           		
-
-           }
-           System.out.println (even + " " + odd);
-       }
-   }
+           int t = Integer.parseInt(br.readLine());
+           while (t-->0){
+               String s = br.readLine();
+               String even = "";
+               String odd = "";
+           
+                for (int i =0; i<s.length(); i++) {
+                    if (i %2 == 0) {
+                        even = even + s.charAt(i);
+                    } else {
+                        odd = odd + s.charAt(i);
+                    }  
+                 }
+                System.out.println(even + " " + odd);
+           }        
+    }
+}
+      
