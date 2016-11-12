e6a01c5e377727a6d7a9d65186d5f4ba19dc1a09
Vimmey <vmy.chopra@gmail.com>
2016-11-12 23:49:24 +0530

    Algorith warmup

--- hackerrank/warmup7.java
+++ hackerrank/warmup7.java
@@ -12,17 +12,22 @@
             int arr[] =new int[3];
             arr[0] = Integer.parseInt(time[0]);
 
-            if (time[2].substring(2,4) == "AM") {
-             System.out.println(time[0] + ":" + time[1] + ":" + time[2].substring(0,2));     
+            if (time[2].substring(2,4).equals("AM")) {
+                if(time[0].equals("12")){
+                      System.out.println("00" + ":" + time[1] + ":" + time[2].substring(0,2));   
+                } else {
+                 System.out.println(time[0] + ":" + time[1] + ":" + time[2].substring(0,2));     
+                }
             } else {
                 if (arr[0] == 12 ) {
-                     System.out.println("00" + ":" + time[1] + ":" + time[2].substring(0,2));   
+                     System.out.println("12" + ":" + time[1] + ":" + time[2].substring(0,2));   
                 } else {
                      int hour = arr[0] + 12;
                      System.out.println(hour+ ":" + time[1] + ":" + time[2].substring(0,2));   
                 }      
             }   
-            
         }
+            
+        
    }
 
