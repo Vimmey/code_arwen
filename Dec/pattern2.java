65eb0d16f9f812c4b487c4d5f9b1e97509fc304a
Vimmey <vmy.chopra@gmail.com>
2016-12-19 23:25:37 +0530

    Patterns

---
+++ Dec/pattern2.java
@@ -0,0 +1,24 @@
+
+// ******
+// *****
+// ****
+// ***
+// **
+// *
+
+import java.util.Scanner;
+ 
+class pattern2
+{
+   public static void main(String args[])
+   {
+      for (int i=0; i<6; i++) {
+         for (int j=6; j>i;j--) {
+              System.out.print("*");
+         }
+         System.out.println();
+      }
+
+   }
+}
+
