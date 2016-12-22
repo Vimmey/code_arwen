4df3518a7e5916e989fdb20d3e50d9523c570b9d
Vimmey <vmy.chopra@gmail.com>
2016-12-22 23:24:46 +0530

    Patterns

---
+++ Dec/pattern5.java
@@ -0,0 +1,23 @@
+
+// 54321
+// 4321
+// 321
+// 21
+// 1
+
+import java.util.Scanner;
+ 
+class pattern5
+{
+   public static void main(String args[])
+   {
+      for (int i=5; i>0; i--) {
+         for (int j=i; j>0;j--) {
+              System.out.print(j);
+         }
+         System.out.println();
+      }
+
+   }
+}
+
